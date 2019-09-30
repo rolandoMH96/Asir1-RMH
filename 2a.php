bdd0597eb6b5fa3bec4581848b7bc3b86a57963f
Rolando Matos <rolandolk15@gmail.com>
2019-09-30 11:14:58 +0200

    version 6

---
+++ 2a.php
@@ -0,0 +1,7 @@
+<?php
+$a=9;
+$b=6;
+echo "suma:" .$a."+".$b."=".($a+$b). '</br>';
+echo "resta:" .$a."-".$b."=".($a-$b). '</br>';
+echo "division:" .$a."/".$b."=".($a/$b). '</br>';
+echo "multiplicacion:" .$a."x".$b."=".($a*$b). '</br>';
