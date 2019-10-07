78a89a0f487edeb090d8a5d4c7d6bbb1de8ee93a
Rolando Matos <rolandolk15@gmail.com>
2019-10-07 13:03:29 +0200

    version 8

--- calendario.php
+++ calendario.php
@@ -1,44 +1,80 @@
 <?php
-$a=[
-	'Enero',
-	'Febrero',
-	'Marzo',
-	'Abril',
-	'Mayo',
-	'Junio',
-	'Julio',
-	'Agosto',
-	'Septiembre',
-	'Octubre',
-	'Noviembre',
-	'Diciembre',
-	];
-	echo '<pre>';
-	print_r ($a);
-	echo '</pre>';
-	for ($i = 0; $i <12; $i++)
-		echo $a[$i]. '<br>';
-foreach ($a as $mes){
-		echo '<pre>';
-		echo $mes;
-		echo'</pre>';
-}
-function mes($i){
+function mes($n){
 	$a=[
-	'Enero',
-	'Febrero',
-	'Marzo',
-	'Abril',
-	'Mayo',
-	'Junio',
-	'Julio',
-	'Agosto',
-	'Septiembre',
-	'Octubre',
-	'Noviembre',
-	'Diciembre',
+	[
+	'mes'=>'Enero',
+	'dias'=>'3',
+	'estacion'=>'Verano'
+	],
+	[
+	'mes'=>'Febrero',
+	'dias'=>'28',
+	'estacion'=>'Verano'
+	],
+	[
+	'mes'=>'Marzo',
+	'dias'=>'31',
+	'estacion'=>'Primavera'
+	],
+	[
+	'mes'=>'Abril',
+	'dias'=>'30',
+	'estacion'=>'Otoño'
+	],
+	
+	[
+	'mes'=>'Mayo',
+	'dias'=>'31',
+	'estacion'=>'Otoño'
+	],
+	[
+	'mes'=>'Junio',
+	'dias'=>'30',
+	'estacion'=>'Verano'
+	],
+	[
+	'mes'=>'Julio',
+	'dias'=>'31',
+	'estacion'=>'Verano'
+	],
+	[
+	'mes'=>'Agosto',
+	'dias'=>'31',
+	'estacion'=>'Verano'
+	],
+	
+	[
+	'mes'=>'Septiembre',
+	'dias'=>'30',
+	'estacion'=>'Verano'
+	],
+	[
+	'mes'=>'Octubre',
+	'dias'=>'31',
+	'estacion'=>'invierno'
+	],
+	[
+	'mes'=>'Noviembre',
+	'dias'=>'30',
+	'estacion'=>'invierno'
+	],
+	[
+	'mes'=>'Diciembre',
+	'dias'=>'31',
+	'estacion'=>'invierno'
+	]
+	
 	];
-	return $a[$i-1];
-}
-echo mes(6);
+	$r=null;
+	$r=$r.$a[$n-1]['mes'].'<br>';
+	$r=$r.$a[$n-1]['dias'].'<br>';
+	$r=$r.$a[$n-1]['estacion'];
+	return $r;
+
+} 
+echo mes (2).'<br>';
+echo mes (5).'<br>';
+echo mes (12).'<br>';
+echo mes (3);
 ?>
+	
