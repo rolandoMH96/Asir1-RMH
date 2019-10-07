6b8d7e2100464ee6108bf03d6bb71385da82f143
Rolando Matos <rolandolk15@gmail.com>
2019-10-07 11:06:40 +0200

    version8

--- else_a.php
+++ else_a.php
@@ -3,12 +3,11 @@ function nota($a){
 	if ($a<5){
 	$r='no apto';
 	}
-	
 	else{ 
 	$r='apto';
 	}
 	return $r;
-
 }
-echo nota(7) .'<br>';
-echo nota(2) .'<br>';
+echo '7'.'='.nota (7).'<br>';
+echo '3'.'='.nota(3);
+?>
