4c8e79c1e1ee828f2659772d3f918f60391503f5
Rolando Matos <rolandolk15@gmail.com>
2019-11-11 11:00:49 +0100

    version 12

--- triangulo_A.php
+++ triangulo_A.php
@@ -1,7 +1,7 @@
 <?php
 	function longitud($a,$b,$c){
 		$r='triangulo escaleno';
-		if($a==$b || $b==$c){
+		if($a==$b|| $a==$c || $b==$c){
 			$r="triangulo Isósceles";
 		
 		if($b==$c and $a==$c)
@@ -9,7 +9,10 @@
 	}
 		return $r;
 	}
+	echo "equilatero"."<br>";
 	echo longitud(5,5,5)."<br>";
+	echo "isosceles"."<br>";
 	echo longitud(2,2,1)."<br>";
+	echo "escaleno"."<br>";
 	echo longitud(2,4,5);
 ?>
