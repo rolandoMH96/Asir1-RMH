a3be88d95769a1f56daf63c3e249bd20521b944e
Rolando Matos <rolandolk15@gmail.com>
2020-01-13 11:00:10 +0100

    version 15

--- html.php
+++ html.php
@@ -26,7 +26,7 @@
 	</ol>
 	<center>
 	<h3>Gatos en internet</h3>
-	<img src="img1.jpg" width="300" height="290"> <img src="img2.jpg" width="300" height="290"> <img src="img4.jpg" width="300" height="290">
+	<img src="img1.jpg" width="300" height="290"/> <img src="img2.jpg" width="300" height="290"/> <img src="img4.jpg" width="300" height="290"/>
 </center>
 <center>
 <h3>¿Que nombres te gustan para los gatos?</h3>
@@ -36,9 +36,9 @@
 <form>
 
  <br> Aqui el nombre:
-<input type="text" name="name"><br/>
+<input type="text" name="name"/><br/>
 <br>
-<input type="submit" name="submit" value="listo"><br/>
+<input type="submit" name="submit" value="listo"/><br/>
 </form>
 </center>
 <br>
@@ -72,16 +72,16 @@
  <center><b><font size="5">Más razones para amar a los gatos</font></b></center>
  <br/>
  <center>
- <img src="img5.jpg" width="300" height="290">  <img src="img6.jpg" width="300" height="290">  <img src="img7.jpg" width="300" height="290">
+ <img src="img5.jpg" width="300" height="290">  <img src="img6.jpg" width="300" height="290">  <img src="img7.jpg" width="300" height="290"/>
  <center/>
  <br>
- <h2>Primer gato famoso</h2>.<img src="img8.jpg" width="300" height="290">
+ <h2>Primer gato famoso</h2>.<img src="img8.jpg" width="300" height="290"/>
  <h3>Nombre: Grumpy cat</h3>
  <h4>Año de origen 2012</h4>
  <br/>
  
  <h2>Famoso de hoy</h2>
- <img src="img9.jpg" width="300" height="290">
+ <img src="img9.jpg" width="300" height="290"/>
  <h3>Nombre: Smudge Lord</h3>
  <h4>Año de origen 2019</h4>
 </body>
